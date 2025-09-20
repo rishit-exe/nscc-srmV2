d68f92be7b56bcdb1e20932530d6b818587b694d
ishangoel2005 <ishangoel2005@gmail.com>
2025-09-20 13:59:13 +0530

    Updated The Members and Positions

--- src/assets/data/2025-26.js
+++ src/assets/data/2025-26.js
@@ -94,19 +94,18 @@ const techTeam = [
         ],
       },
       {
-        name: "Abhiraj Bhowmick",
-        designation: "Team Member",
-        bio: "Carpe Diem",
+        name: "Ishan Goel",
+        designation: "Associate",
+        bio: "Building smooth, scalable and sleek websites — turning ideas into digital experiences that leave a lasting impact.",
         social: [
-          { name: "github", url: "https://github.com/RainboeStrykr" },
-          { name: "instagram", url: "https://www.instagram.com/abhiraj.fr/" },
-          { name: "linkedin", url: "https://www.linkedin.com/in/abhiraj-bhowmick/" },
-          { name: "other_link", url: "https://abhiraj.co/" },
+          { name: "github", url: "https://github.com/ishangoel2005" },
+          { name: "instagram", url: "https://www.instagram.com/ishan.goel2005?igsh=MTE1NXN1NmN1eXF4MQ==" },
+          { name: "linkedin", url: "https://www.linkedin.com/in/ishangoel2005/" },
         ],
       },
       {
         name: "Vidhi Sharma",
-        designation: "Team Member",
+        designation: "Associate",
         bio: "Fluent in emojis, errors, and enthusiasm",
         social: [
           { name: "github", url: "https://github.com/vidhisharma1212" },
@@ -115,13 +114,14 @@ const techTeam = [
         ],
       },
       {
-        name: "Shasmitha R",
+        name: "Abhiraj Bhowmick",
         designation: "Team Member",
-        bio: "Running on logic, caffeine, and the occasional ‘aha!’ moment",
+        bio: "Carpe Diem",
         social: [
-          { name: "github", url: "https://github.com/shasmithareddy" },
-          { name: "instagram", url: "https://www.instagram.com/_s.h.a.s.m.i.t.h.a._r.e.d.d.y_/" },
-          { name: "linkedin", url: "https://www.linkedin.com/in/shasmitha-r-7ba139177/" },
+          { name: "github", url: "https://github.com/RainboeStrykr" },
+          { name: "instagram", url: "https://www.instagram.com/abhiraj.fr/" },
+          { name: "linkedin", url: "https://www.linkedin.com/in/abhiraj-bhowmick/" },
+          { name: "other_link", url: "https://abhiraj.co/" },
         ],
       },
       {
@@ -146,27 +146,6 @@ const techTeam = [
           { name: "other_link", url: "https://hridayesh11.github.io" },
         ],
       },
-      {
-        name: "Sudhir Singh",
-        designation: "Team Member",
-        bio: "B.Tech CSE-CN student passionate about Data Science and building impactful tech solutions.",
-        social: [
-          { name: "github", url: "https://github.com/SudhirSingh62" },
-          { name: "instagram", url: "https://www.instagram.com/Sudhir_singh.18" },
-          { name: "linkedin", url: "https://www.linkedin.com/in/sudhir-singh-840603250" },
-          { name: "other_link", url: "https://sudhirsingh62.github.io/portfolio" },
-        ],
-      },
-      {
-        name: "Ishan Goel",
-        designation: "Team Member",
-        bio: "Building smooth, scalable and sleek websites — turning ideas into digital experiences that leave a lasting impact.",
-        social: [
-          { name: "github", url: "https://github.com/ishangoel2005" },
-          { name: "instagram", url: "https://www.instagram.com/ishan.goel2005?igsh=MTE1NXN1NmN1eXF4MQ==" },
-          { name: "linkedin", url: "https://www.linkedin.com/in/ishangoel2005/" },
-        ],
-      },
     ],
   },
 ];
@@ -177,14 +156,13 @@ const nonTechTeam = [
     category: "Non Tech",
     members: [
       {
-        name: "Aayushman Kathayat",
+        name: "Vansh Sarbhai",
         designation: "Director",
-        bio: "Thinks in strategy, speaks in results, and leads with command — fueled by purpose, forged in persistence.",
+        bio: "I find ur lack of faith disturbing",
         social: [
-          { name: "github", url: "https://github.com/aayushman2727" },
-          { name: "instagram", url: "https://www.instagram.com/aayushman_k27?igsh=MWE0eGcwcWFqMnUwYQ==" },
-          { name: "linkedin", url: "http://www.linkedin.com/in/aayushman-kathayat-104768295/" },
-          { name: "other_link", url: "http://www.linkedin.com/in/aayushman-kathayat-104768295/" },
+          { name: "github", url: "https://share.google/MUhz6qFu74ZZQHhRA" },
+          { name: "instagram", url: "https://www.instagram.com/vansh_sarbhai?igsh=ZHB4NzhlMzZ3cGti" },
+          { name: "linkedin", url: "https://www.linkedin.com/in/vansh-sarbhai-1869a6293" },
         ],
       },
       {
@@ -198,13 +176,13 @@ const nonTechTeam = [
         ],
       },
       {
-        name: "Vansh Sarbhai",
+        name: "Sukhad Kaur",
         designation: "Associate Director",
-        bio: "I find ur lack of faith disturbing",
+        bio: "Work in progress fueled by logic, learning, and chai..",
         social: [
-          { name: "github", url: "https://share.google/MUhz6qFu74ZZQHhRA" },
-          { name: "instagram", url: "https://www.instagram.com/vansh_sarbhai?igsh=ZHB4NzhlMzZ3cGti" },
-          { name: "linkedin", url: "https://www.linkedin.com/in/vansh-sarbhai-1869a6293" },
+          { name: "github", url: "https://github.com/sk01410" },
+          { name: "instagram", url: "https://www.instagram.com/sukhi_k_14/profilecard/?igsh=aG93MzNjY2p2MzA1" },
+          { name: "linkedin", url: "https://www.linkedin.com/in/sukhadkaur/" },
         ],
       },
       {
@@ -240,24 +218,13 @@ const nonTechTeam = [
         ],
       },
       {
-        name: "Amrita Hariharan",
-        designation: "Team Member",
-        bio: "Something's always cooking in my brain",
+        name: "Rakhshanda Yadav",
+        designation: "Associate",
+        bio: "Rakhshanda Yadav, a confident, resilient, and ambitious B.Tech 2nd-year student, always ready to embrace challenges and push boundaries.",
         social: [
-          { name: "github", url: "https://github.com/amritaa1106" },
-          { name: "instagram", url: "https://www.instagram.com/amrita_hariharan_?igsh=MW1wcjI1cW4yNGVwdw%3D%3D&utm_source=qr" },
-          { name: "linkedin", url: "https://www.linkedin.com/in/amrita-hariharan2205/" },
-        ],
-      },
-      {
-        name: "Tusharika Suman",
-        designation: "Team Member",
-        bio: "Fueled by ambition, creativity, and good music.",
-        social: [
-          { name: "github", url: "https://github.com/tusharikasuman" },
-          { name: "instagram", url: "https://www.instagram.com/tusharika.jpeg/" },
-          { name: "linkedin", url: "https://www.linkedin.com/in/tusharika-suman-328497270/" },
-          { name: "other_link", url: "https://portfolio-website-kappa-murex-17.vercel.app/" },
+          { name: "github", url: "https://github.com/Rakhshandaa" },
+          { name: "instagram", url: "https://www.instagram.com/rakhshandayadav?igsh=Y3Ywa3Q3dW5kOGI2&utm_source=qr" },
+          { name: "linkedin", url: "https://linkedin.com/in/rakhshanda-yadav-2109b8324" },
         ],
       },
       {
@@ -271,46 +238,6 @@ const nonTechTeam = [
         ],
       },
       {
-        name: "Purva Jain",
-        designation: "Team Member",
-        bio: "“Driven by curiosity, shaped by creativity, and grounded in collaboration.”",
-        social: [
-          { name: "github", url: "https://github.com/purvajain-git" },
-          { name: "instagram", url: "https://instagram.com/purvajain_17" },
-          { name: "linkedin", url: "https://www.linkedin.com/in/purva-jain17?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" },
-        ],
-      },
-      {
-        name: "Krishnansh Pangasa",
-        designation: "Team Member",
-        bio: "Building dreams one step at a time",
-        social: [
-          { name: "github", url: "https://github.com/krishnansh2785" },
-          { name: "instagram", url: "https://www.instagram.com/krishnansh_27?igsh=MWVlaTNnN2h5cGptYw%3D%3D&utm_source=qr" },
-          { name: "linkedin", url: "https://www.linkedin.com/in/krishnansh-pangasa-39657a2ab" },
-        ],
-      },
-      {
-        name: "Ritika Sinha",
-        designation: "Team Member",
-        bio: "Learning. Unlearning. Becoming.",
-        social: [
-          { name: "github", url: "https://github.com/ritikaonlyy" },
-          { name: "instagram", url: "https://www.instagram.com/ritikaonlyy?igsh=MXVjZjhybWVlOGk4eQ%3D%3D&utm_source=qr" },
-          { name: "linkedin", url: "https://www.linkedin.com/in/ritikaonlyy" },
-        ],
-      },
-      {
-        name: "Sukhad Kaur",
-        designation: "Team Member",
-        bio: "Work in progress fueled by logic, learning, and chai..",
-        social: [
-          { name: "github", url: "https://github.com/sk01410" },
-          { name: "instagram", url: "https://www.instagram.com/sukhi_k_14/profilecard/?igsh=aG93MzNjY2p2MzA1" },
-          { name: "linkedin", url: "https://www.linkedin.com/in/sukhadkaur/" },
-        ],
-      },
-      {
         name: "Shambhavi Akhouri",
         designation: "Team Member",
         bio: "Thinks like a developer, acts like a strategist, and delivers like a leader.",
@@ -318,16 +245,6 @@ const nonTechTeam = [
           { name: "github", url: "https://github.com/shambhaviakhouri" },
           { name: "instagram", url: "https://www.instagram.com/shambhavi_ak?igsh=M3NjY2lyM2Uwd3pz" },
           { name: "linkedin", url: "https://www.linkedin.com/in/shambhavi-akhouri-1979b5283" },
-        ],
-      },
-      {
-        name: "Rakhshanda Yadav",
-        designation: "Team Member",
-        bio: "Rakhshanda Yadav, a confident, resilient, and ambitious B.Tech 2nd-year student, always ready to embrace challenges and push boundaries.",
-        social: [
-          { name: "github", url: "https://github.com/Rakhshandaa" },
-          { name: "instagram", url: "https://www.instagram.com/rakhshandayadav?igsh=Y3Ywa3Q3dW5kOGI2&utm_source=qr" },
-          { name: "linkedin", url: "https://linkedin.com/in/rakhshanda-yadav-2109b8324" },
         ],
       },
       {
@@ -351,6 +268,16 @@ const creatives = [
     category: "Creatives",
     members: [
       {
+        name: "Abhishek Kinjawadekar",
+        designation: "Director",
+        bio: "You ain't gotta lie",
+        social: [
+          { name: "github", url: "https://github.com/Abhisheksk2005" },
+          // { name: "instagram", url: "https://www.instagram.com/shreya.nair._/" },
+          { name: "linkedin", url: "https://www.linkedin.com/in/abhishekkinjawadekar?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
+        ],
+      },
+      {
         name: "Shreya Nair",
         designation: "Associate Director",
         bio: "A curious mind with a passion for learning and creating.",
@@ -373,7 +300,7 @@ const creatives = [
       
       {
         name: "Keshav Gehlot",
-        designation: "Team Member",
+        designation: "Associate",
         bio: `"If I want to, I will. If I don't, I won't."`,
         social: [
           { name: "github", url: "https://github.com/Keshav-gehlot" },
@@ -384,7 +311,7 @@ const creatives = [
       },
       {
         name: "Praneet Rathore",
-        designation: "Team Member",
+        designation: "Associate",
         bio: "Heart full of hope & love and trusting God's plan.",
         social: [
           { name: "github", url: "https://github.com/Praneet6" },
@@ -393,13 +320,13 @@ const creatives = [
         ],
       },
       {
-        name: "Ashmit Singh",
+        name: "Anurag Patel",
         designation: "Team Member",
-        bio: "Visual storyteller shaping narratives through video",
+        bio: "",
         social: [
-          { name: "github", url: "https://github.com/xxxASHxxx" },
-          { name: "instagram", url: "https://www.instagram.com/ashmit_fr?igsh=NDRhazJsYWZjOTg3" },
-          { name: "linkedin", url: "https://www.linkedin.com/in/ashmitsinghixd" },
+          { name: "github", url: "" },
+          { name: "instagram", url: "" },
+          { name: "linkedin", url: "" },
         ],
       },
     ],
